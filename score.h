bef013213bf5c11994a6277db7a3edf390cd437a
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 13:29:10 -0700

    Update score.h
    Implement class

--- score.h
+++ score.h
@@ -1,4 +1,17 @@
 #ifndef SCORE_H
 #define SCORE_H
 
+#include <QGraphicsTextItem>
+
+class score : public QGraphicsTextItem {
+public:
+    score(QGraphicsItem * parent = 0);
+    void increase();
+    int getScore();
+
+public slots:
+    int scores;
+};
+
+
 #endif // SCORE_H
