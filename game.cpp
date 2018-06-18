df8851eea4d2439599de73903db945d5b53d640f
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 16:44:37 -0700

    Update game.cpp
    fixing spacing and removing comments

--- game.cpp
+++ game.cpp
@@ -9,10 +9,8 @@
 Game::Game(QWidget *parent){
     // create the scene
     scene = new QGraphicsScene();
-    scene->setSceneRect(0,0,800,600); // make the scene 800x600 instead of infinity by infinity (default)
+    scene->setSceneRect(0,0,800,600);
 
-    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
-    // it can be used to visualize scenes)
     setScene(scene);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -20,10 +18,12 @@ Game::Game(QWidget *parent){
 
     // create the player
     player = new character();
-    player->setPos(400,500); // TODO generalize to always be in the middle bottom of screen
+    player->setPos(400,500); 
+    
     // make the player focusable and set it to be the current focus
     player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
+    
     // add the player to the scene
     scene->addItem(player);
     
