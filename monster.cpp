afd633915ead2f54892aca0e285320165a414b39
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 12:45:36 -0700

    Update monster.cpp
    Copying code over from beam.cpp but changing it for monster.cpp

--- monster.cpp
+++ monster.cpp
@@ -1,1 +1,28 @@
 #include "monster.h"
+#include <QTimer>
+#include <QGraphicsScene>
+
+monster::monster()
+{
+    //set size of beam
+    setRect(0,0,10,50);
+
+    //connect
+    QTimer * timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
+
+    //beam moves every 50 milisec
+    timer->start(50);
+}
+
+void monster::move(){
+    //move beam up
+    setPos(x(),y() - 10);
+
+    //delete beam if it does out of view
+    if(pos().y() + rect().height() < 0){
+        scene()->removeItem(this);
+        delete this;
+    }
+
+}
