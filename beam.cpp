84318bb0d37d2836c70e481b69e7b0d7050bfbf2
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 04:25:49 -0700

    Code for beam constructer
    Implementation of constructor

--- beam.cpp
+++ beam.cpp
@@ -3,6 +3,15 @@
 
 beam::beam()
 {
+   //set size of beam
+    setRect(0,0,10,50);
+
+    //connect
+    QTimer * timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
+
+    //beam moves every 50 milisec
+    timer->start(50);
 }
 
 void beam::move(){
