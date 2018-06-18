f47fcd3ab9b368eaa29328d7788c495cbdec8eca
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 15:14:50 -0700

    Update game.h
    -appropriate header files 
    -make pointers

--- game.h
+++ game.h
@@ -1,4 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "character.h"
+#include "score.h"
+#include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+class Game : public QGraphicsView {
+
+public:
+    Game(QWidget * parent = 0);
+
+    QGraphicsScene * scene;
+    character * player;
+};
+
+
 #endif // GAME_H
