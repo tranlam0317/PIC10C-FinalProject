e68bf61ed790023ff74e9945df1642799a20ef34
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 04:23:13 -0700

    Update beam.h
    Create beam class with constructor and move function (public slot)

--- beam.h
+++ beam.h
@@ -1,4 +1,18 @@
 #ifndef BEAM_H
 #define BEAM_H
 
+#include <QGraphicsRectItem>
+#include <QObject>
+
+class beam: public QObject, public QGraphicsRectItem{
+    Q_OBJECT
+
+public:
+    beam();
+
+public slots:
+    void move();
+};
+
+
 #endif // BEAM_H
