fa159efabb956c8b89fb61062cdfe24f53edbe1b
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 12:43:20 -0700

    Update monster.h
    Create monster class with appropriate header files

--- monster.h
+++ monster.h
@@ -1,4 +1,18 @@
 #ifndef MONSTER_H
 #define MONSTER_H
 
+#include <QGraphicsRectItem>
+#include <QObject>
+
+class monster: public QObject, public QGraphicsRectItem{
+  Q_OBJECT 
+    
+public:
+  monster(); 
+  
+public slots: 
+  void move(); 
+};
+  
+
 #endif // MONSTER_H
