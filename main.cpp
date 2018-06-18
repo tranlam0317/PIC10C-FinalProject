3219467b4e533bc526dd2994c226af4fc27902b3
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 15:16:31 -0700

    Update main.cpp
    -making main point to game.cpp

--- main.cpp
+++ main.cpp
@@ -5,44 +5,14 @@
 #include <QGraphicsView>
 #include <QTimer> 
 
-score * Score;
+Game * game;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    
+    game = new Game();
+    game->show();
 
-    //adding a player and displaying it
-    QGraphicsScene * scene = new QGraphicsScene();
-    character * player = new character();
-    player->setRect(0,0,100,100);
-    scene->addItem(player);
-    
-     //making player focused
-    player->setFlag((QGraphicsItem::ItemIsFocusable));
-    player->setFocus();
-    
-    //displaying player
-    QGraphicsView * view = new QGraphicsView(scene);
-
-    //disabling scroll bars
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-    //show the view
-    view->show();
-    view->setFixedSize(800,600);
-    scene->setSceneRect(0,0,800,600);
-
-    player->setPos(view->width()/2, view->height() - player->rect().height());
-    
-    //spawn monsters 
-    QTimer * timer = new QTimer(); 
-    QObject:: connect(timer, SIGNAL(timeout()), player, SLOT(spawn()));
-    timer->start(2500);
-    
-    //create a score
-    score * Score = new score();
-    scene ->addItem(Score);
-    
     return a.exec();
 }
