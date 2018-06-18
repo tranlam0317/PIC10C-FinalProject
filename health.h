c83735c75842fdcbffe3d5511fb4c4bda879108e
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 15:25:45 -0700

    Update health.h
    same class as score but for health 
    -decrease function

--- health.h
+++ health.h
@@ -1,4 +1,18 @@
 #ifndef HEALTH_H
 #define HEALTH_H
 
+#include <QGraphicsTextItem>
+
+class Health:public QGraphicsTextItem {
+
+public:
+    Health(QGraphicsItem * parent = 0);
+    void decrease();
+    int getHealth();
+
+private:
+    int health; //tracks current score
+};
+
+
 #endif // HEALTH_H
