65b3ac52ed65f269908dd96095d41cfe177ca4b0
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 15:15:31 -0700

    Update game.cpp
    -moving everything that was in main to game.cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,38 @@
+#include <QApplication>
+#include "game.h"
+#include "character.h"
+#include "score.h"
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include <QTimer>
+
+Game::Game(QWidget *parent){
+    // create the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600); // make the scene 800x600 instead of infinity by infinity (default)
+
+    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
+    // it can be used to visualize scenes)
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+
+    // create the player
+    player = new character();
+    player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
+    player->setPos(400,500); // TODO generalize to always be in the middle bottom of screen
+    // make the player focusable and set it to be the current focus
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+    // add the player to the scene
+    scene->addItem(player);
+
+    // spawn enemies
+    QTimer * timer = new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
+    timer->start(2000);
+
+    show();
+}
 
