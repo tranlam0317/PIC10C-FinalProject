e375f7c3796fd1212f34b11a27f2e974639ead65
tranlam0317 <38049794+tranlam0317@users.noreply.github.com>
2018-06-18 04:27:31 -0700

    Update character.cpp
    -Code for space button 
    -Appropriate header files added

--- character.cpp
+++ character.cpp
@@ -1,12 +1,22 @@
 #include "character.h"
+#include "beam.h"
+#include "QGraphicsScene"
 #include <QKeyEvent>
 
 void character:: keyPressEvent(QKeyEvent *event){
+    //left and right movement 
     if(event->key() == Qt::Key_Left){
         setPos(x() - 10, y());
     }
 
     else if(event->key() == Qt::Key_Right){
         setPos(x() + 10, y());
+    }
+    
+    //space button pressed
+    else if(event->key() == Qt::Key_Space){
+        beam * Beam = new beam();
+        Beam->setPos(x(),y());
+        scene()->addItem(Beam);
     }
 }
